7903be577d1a43261dcb5df332b9e08b49a68829
damianwap <54344687+damianwap@users.noreply.github.com>
2019-10-29 22:41:52 +0700

    Manajemen Kategori
    Kurang login dan manajemen user

---
+++ src/main/java/id/ac/ukdw/Koneksi/Konek.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.ukdw.Koneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+/**
+ *
+ * @author DAMIAN
+ */
+public class Konek {
+    private static Connection conn = null;
+    public static Connection getConnect(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:D:\\TA_RPL\\MakanKuy.db");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            System.out.println("Database tidak ditemukan");
+        }
+        return conn;
+    }
+}
